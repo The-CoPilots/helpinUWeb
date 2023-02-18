30665b638cb7481fd5387de618df2db4e8bb40c1
Samarth Verma <samarthverma@icloud.com>
2023-02-19 06:30:45 +0800

    Multiple changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,34 @@
+import Banner from "@/components/Banner";
 import Hero from "@/components/Hero";
 import Listings from "@/components/Listings";
 
 export default function Home() {
   return (
-    <div className="w-full space-y-12 container mx-auto">
-      <Hero />
-      <div>
-        <div className="text-center text-2xl font-semibold tracking-wide">
-          Categories
+    <>
+      <Banner />
+      <div className="w-full space-y-12 container mx-auto pt-8">
+        <Hero />
+        <div>
+          <div className="text-center text-2xl font-semibold tracking-wide">
+            Categories
+          </div>
+        </div>
+        <div className="grid grid-cols-3 gap-12 max-w-screen-xl mx-auto">
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <div>
+          <h2 className="text-2xl tracking-tight font-bold">
+            Recent opportunities
+          </h2>
+        </div>
+        <div className="flex gap-12">
+          <Listings />
+          <div className="w-80 h-120 bg-gradient-to-tl from-red-200 to-red-600"></div>
         </div>
       </div>
-      <div className="grid grid-cols-3 gap-12 max-w-screen-xl mx-auto">
-        <Card />
-        <Card />
-        <Card />
-      </div>
-      <div>
-        <h2 className="text-2xl tracking-tight font-bold">
-          Recent opportunities
-        </h2>
-      </div>
-      <div className="flex gap-12">
-        <Listings />
-        <div className="w-80 h-120 bg-gradient-to-tl from-red-200 to-red-600"></div>
-      </div>
-    </div>
+    </>
   );
 }
 
