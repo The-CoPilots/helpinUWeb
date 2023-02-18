90fec9f3f5fa85fa5aaf032c114a10f5e74e7a71
Samarth Verma <samarthverma@icloud.com>
2023-02-19 00:31:40 +0800

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+   <div className="container mx-auto mt-12">
+      <div>
+        <h2 className="text-2xl tracking-tight font-bold">Recent opportunities!</h2>
+      </div>
+    </div>
+  )
+}
